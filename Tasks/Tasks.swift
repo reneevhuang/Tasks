6fb9c2dbc2ca7684951aaedb73b466502e3392ec
Renee Huang <renee.v.huang@gmail.com>
2020-09-05 19:56:31 -0500

    Finished basic outline of todo app
    Need ot work on saving data

--- Tasks/Tasks.swift
+++ Tasks/Tasks.swift
@@ -8,7 +8,8 @@
 
 import Foundation
 
-class ToDoItem{
+class ToDoItem: NSObject, NSCoding
+{
     var title: String
     var done: Bool
     
@@ -16,6 +17,38 @@ class ToDoItem{
     {
         self.title=title
         self.done=false
+    }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+    // Try to unserialize the "title" variable
+        if let title = aDecoder.decodeObject(value(forKey: "title") as? String)
+        {
+            self.title = title
+        }
+        else
+        {
+            // There were no objects encoded with the key "title",
+            // so that's an error.
+            return nil
+        }
+        
+        // Check if the key "done" exists, since decodeBool() always succeeds
+        if aDecoder.containsValue(forKey: "done")
+        {
+            self.done = aDecoder.decodeBool(forKey: "done")
+        }
+        else
+        {
+            return nil
+        }
+    }
+        
+    func encode(with aCoder: NSCoder)
+    {
+        // Store the objects into the coder object
+        aCoder.encode(self.title, forKey: "title")
+        aCoder.encode(self.done, forKey: "done")
     }
 }
 
